0206f6ad2a1c2630af95decddab0d0503fd90a0c
Eugene Baranchuk <vencedor@gmail.com>
2015-12-25 15:14:00 +0200

    initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-# color-math
-
-Description.
-
--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="node/node.d.ts" />
-/// <reference path="chroma-js/chroma-js.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
-/// <reference path="fs-extra/fs-extra.d.ts" />
-/// <reference path="cli-color/cli-color.d.ts" />
